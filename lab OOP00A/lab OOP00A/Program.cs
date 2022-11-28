7a7b5ce9534c3c7d60b3f16c1f5d39331504c609
ThanakonSA <119285334+ThanakonSA@users.noreply.github.com>
2022-11-28 15:41:04 +0700

    Add Lab 0A

---
+++ lab OOP00A/lab OOP00A/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+// print string to commanline
+Console.WriteLine("Thanakon Sawangsungnoen");
+// id
+Console.WriteLine("653450285-1");
+// Email
+Console.WriteLine("Thanakon.sa@kkumail.com");
